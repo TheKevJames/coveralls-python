b3057fecbe7ef56ef0d5eeb13ff6ab26cee60173
Ilya Baryshev <baryshev@gmail.com>
2015-08-14 18:23:50 +0300

    Remove python 3.2 from tox testing

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ class PyTest(TestCommand):
 
 setup(
     name='coveralls',
-    version='1.0a2',
+    version='1.0b1',
     packages=['coveralls'],
     url='http://github.com/coagulant/coveralls-python',
     license='MIT',
@@ -45,7 +45,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation :: PyPy',
