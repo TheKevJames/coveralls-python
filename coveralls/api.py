d9f6be1d803f07739c548507bbb024da1e25395f
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2016-03-03 21:40:50 +0530

    Send boolean True. Not string.

--- coveralls/api.py
+++ coveralls/api.py
@@ -73,8 +73,9 @@ class Coveralls(object):
         if os.environ.get('COVERALLS_REPO_TOKEN', None):
             self.config['repo_token'] = os.environ.get('COVERALLS_REPO_TOKEN')
 
-        if os.environ.get('COVERALLS_PARALLEL', False):
-            self.config['parallel'] = os.environ.get('COVERALLS_PARALLEL')
+        parallel = os.environ.get('COVERALLS_PARALLEL', '')
+        if parallel.lower() == 'true':
+            self.config['parallel'] = True
 
         if token_required and not self.config.get('repo_token') and not is_travis_or_circle:
             raise CoverallsException(
