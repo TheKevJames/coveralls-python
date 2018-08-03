ddb99442ca5d662f21de59414d4a95b2580a7284
Update ignore <>
2018-08-03 19:20:41 +0200

    More whitespace

--- tests/git_test.py
+++ tests/git_test.py
@@ -24,25 +24,25 @@ GIT_URL = 'https://github.com/username/Hello-World.git'
 class GitTest(unittest.TestCase):
     def setUp(self):
         self.dir = tempfile.mkdtemp()
-
+        
         sh.cd(self.dir)
         sh.touch('README')
-
+        
         sh.git.init()
         sh.git.config('user.name', '"{}"'.format(GIT_NAME))
         sh.git.config('user.email', '"{}"'.format(GIT_EMAIL))
         sh.git.add('README')
         sh.git.commit('-m', GIT_COMMIT_MSG)
         sh.git.remote('add', GIT_REMOTE, GIT_URL)
-
+    
     def tearDown(self):
         shutil.rmtree(self.dir)
-
+    
     @mock.patch.dict(os.environ, {'TRAVIS_BRANCH': 'master'}, clear=True)
     def test_git(self):
         git_info = coveralls.git.git_info()
         commit_id = git_info['git']['head'].pop('id')
-
+        
         assert re.match(r'^[a-f0-9]{40}$', commit_id)
         assert git_info == {
             'git': {
@@ -69,11 +69,11 @@ class GitLogTest(GitTest):
         
         git_info = gitlog('%H')
         assert re.match(r'^[a-f0-9]{40}$', git_info)
-
+        
         assert gitlog('%aN') == GIT_NAME
         assert gitlog('%ae') == GIT_EMAIL
         assert gitlog('%cN') == GIT_NAME
-        assert gitlog('%ce')== GIT_EMAIL
+        assert gitlog('%ce') == GIT_EMAIL
         assert gitlog('%s') == GIT_COMMIT_MSG
 
 
@@ -99,14 +99,13 @@ class GitInfoTestEnvVars(unittest.TestCase):
         'GIT_COMMITTER_EMAIL':correct_encoding_for_envvars(GIT_EMAIL),
         'GIT_MESSAGE': correct_encoding_for_envvars(GIT_COMMIT_MSG),
         'GIT_URL': correct_encoding_for_envvars(GIT_URL),
-        'GIT_REMOTE': correct_encoding_for_envvars(GIT_REMOTE), 
-    }, 
-    clear=True)
+        'GIT_REMOTE': correct_encoding_for_envvars(GIT_REMOTE),
+    }, clear=True)
     def test_gitlog_envvars(self):
         git_info = coveralls.git.git_info()
         commit_id = git_info['git']['head'].pop('id')
         assert re.match(r'^[a-f0-9]{40}$', commit_id)
-
+        
         assert git_info == {
             'git': {
                 'head': {
