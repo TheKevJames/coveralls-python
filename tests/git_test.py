312e09f164c871200111a4aac3572e8c5fe30d88
Update ignore <>
2018-08-04 11:32:21 +0200

    More whitespace fixes.

--- tests/git_test.py
+++ tests/git_test.py
@@ -24,25 +24,25 @@ GIT_URL = 'https://github.com/username/Hello-World.git'
 class GitTest(unittest.TestCase):
     def setUp(self):
         self.dir = tempfile.mkdtemp()
-        
+
         sh.cd(self.dir)
         sh.touch('README')
-        
+
         sh.git.init()
         sh.git.config('user.name', '"{}"'.format(GIT_NAME))
         sh.git.config('user.email', '"{}"'.format(GIT_EMAIL))
         sh.git.add('README')
         sh.git.commit('-m', GIT_COMMIT_MSG)
         sh.git.remote('add', GIT_REMOTE, GIT_URL)
-    
+
     def tearDown(self):
         shutil.rmtree(self.dir)
-    
+
     @mock.patch.dict(os.environ, {'TRAVIS_BRANCH': 'master'}, clear=True)
     def test_git(self):
         git_info = coveralls.git.git_info()
         commit_id = git_info['git']['head'].pop('id')
-        
+
         assert re.match(r'^[a-f0-9]{40}$', commit_id)
         assert git_info == {
             'git': {
@@ -66,10 +66,10 @@ class GitLogTest(GitTest):
 
     def test_gitlog(self):
         gitlog = coveralls.git.gitlog
-        
+
         git_info = gitlog('%H')
         assert re.match(r'^[a-f0-9]{40}$', git_info)
-        
+
         assert gitlog('%aN') == GIT_NAME
         assert gitlog('%ae') == GIT_EMAIL
         assert gitlog('%cN') == GIT_NAME
@@ -105,7 +105,7 @@ class GitInfoTestEnvVars(unittest.TestCase):
         git_info = coveralls.git.git_info()
         commit_id = git_info['git']['head'].pop('id')
         assert re.match(r'^[a-f0-9]{40}$', commit_id)
-        
+
         assert git_info == {
             'git': {
                 'head': {
