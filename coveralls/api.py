be7e6bba8f2ea87a7025a8b2d0ca16bfbaece60f
Nick Merwin <n@mer.io>
2019-04-29 12:18:21 -0700

    more linting

--- coveralls/api.py
+++ coveralls/api.py
@@ -171,7 +171,8 @@ class Coveralls(object):
 
         endpoint = '{}/api/v1/jobs'.format(self._coveralls_host.rstrip('/'))
         verify = not bool(os.environ.get('COVERALLS_SKIP_SSL_VERIFY'))
-        response = requests.post(endpoint, files={'json_file': json_string}, verify=verify)
+        response = requests.post(endpoint, files={'json_file': json_string},
+                                 verify=verify)
         try:
             response.raise_for_status()
             return response.json()
