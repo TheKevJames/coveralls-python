749b97fe5f38765251628a673aaf6baca096e2c5
Kevin James <kevin@talkiq.com>
2019-05-07 17:41:42 -0700

    fix(dependencies): match urllib3 pin for py2 to requests (#200)

--- setup.py
+++ setup.py
@@ -36,7 +36,11 @@ setup(
     tests_require=['mock', 'pytest', 'sh>=1.08'],
     extras_require={
         'yaml': ['PyYAML>=3.10'],
-        ':python_version < "3"': ['urllib3[secure]'],
+        # See https://github.com/kennethreitz/requests/issues/5067
+        # We need to stay aligned with requests' pin to ensure urllib3 updates
+        # don't break pip installation.
+        # See https://github.com/kennethreitz/requests/blob/master/setup.py
+        ':python_version < "3"': ['urllib3[secure]>=1.21.1,<1.25'],
     },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
