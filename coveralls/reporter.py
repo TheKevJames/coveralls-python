bcac2377c974f6d2aaf935d9d96bba173ccc9d78
Kevin James <KevinJames@thekev.in>
2019-12-30 18:42:31 -0600

    feat: support for coverage5

--- coveralls/reporter.py
+++ coveralls/reporter.py
@@ -21,6 +21,86 @@ class CoverallReporter(object):
         self.coverage = []
         self.report(cov, conf)
 
+    def report5(self, cov):
+        # N.B. this method is 99% copied from the coverage source code;
+        # unfortunately, the coverage v5 style of `get_analysis_to_report`
+        # errors out entirely if any source file has issues -- which would be a
+        # breaking change for us. In the interest of backwards compatibility,
+        # I've copied their code here so we can maintain the same `coveralls`
+        # API regardless of which `coverage` version is being used.
+        #
+        # TODO: deprecate the relevant APIs so we can just use the coverage
+        # public API directly.
+        #
+        # from coverage.report import get_analysis_to_report
+        # try:
+        #     for cu, analyzed in get_analysis_to_report(cov, None):
+        #         self.parse_file(cu, analyzed)
+        # except NoSource:
+        #     # Note that this behavior must necessarily change between
+        #     # coverage<5 and coverage>=5, as we are no longer interweaving
+        #     # with get_analysis_to_report (a single exception breaks the
+        #     # whole loop)
+        #     log.warning('No source for at least one file')
+        # except NotPython:
+        #     # Note that this behavior must necessarily change between
+        #     # coverage<5 and coverage>=5, as we are no longer interweaving
+        #     # with get_analysis_to_report (a single exception breaks the
+        #     # whole loop)
+        #     log.warning('A source file is not python')
+        # except CoverageException as e:
+        #     if str(e) != 'No data to report.':
+        #         raise
+
+        from coverage.files import FnmatchMatcher, prep_patterns
+
+        # get_analysis_to_report starts here; changes marked with TODOs
+        file_reporters = cov._get_file_reporters(None)  # pylint: disable=W0212
+        config = cov.config
+
+        if config.report_include:
+            matcher = FnmatchMatcher(prep_patterns(config.report_include))
+            file_reporters = [fr for fr in file_reporters
+                              if matcher.match(fr.filename)]
+
+        if config.report_omit:
+            matcher = FnmatchMatcher(prep_patterns(config.report_omit))
+            file_reporters = [fr for fr in file_reporters
+                              if not matcher.match(fr.filename)]
+
+        # TODO: deprecate changes
+        # if not file_reporters:
+        #     raise CoverageException("No data to report.")
+
+        for fr in sorted(file_reporters):
+            try:
+                analysis = cov._analyze(fr)  # pylint: disable=W0212
+            except NoSource:
+                if not config.ignore_errors:
+                    # TODO: deprecate changes
+                    # raise
+                    log.warning('No source for %s', fr.filename)
+            except NotPython:
+                # Only report errors for .py files, and only if we didn't
+                # explicitly suppress those errors.
+                # NotPython is only raised by PythonFileReporter, which has a
+                # should_be_python() method.
+                if fr.should_be_python():
+                    if config.ignore_errors:
+                        msg = "Couldn't parse Python file '{}'".format(
+                            fr.filename)
+                        cov._warn(msg,  # pylint: disable=W0212
+                                  slug="couldnt-parse")
+                    else:
+                        # TODO: deprecate changes
+                        # raise
+                        log.warning('Source file is not python %s',
+                                    fr.filename)
+            else:
+                # TODO: deprecate changes (well, this one is fine /shrug)
+                # yield (fr, analysis)
+                self.parse_file(fr, analysis)
+
     def report(self, cov, conf, morfs=None):
         """
         Generate a part of json report for coveralls
@@ -28,11 +108,12 @@ class CoverallReporter(object):
         `morfs` is a list of modules or filenames.
         `outfile` is a file object to write the json to.
         """
+        # pylint: disable=too-many-branches
         try:
             from coverage.report import Reporter
             self.reporter = Reporter(cov, conf)
         except ImportError:  # coverage >= 5.0
-            raise Exception('TODO: coverage v5 compatibility')
+            return self.report5(cov)
 
         units = None
         if hasattr(self.reporter, 'find_code_units'):
@@ -108,7 +189,14 @@ class CoverallReporter(object):
         if not analysis.has_arcs():
             return None
 
-        branch_lines = analysis.branch_lines()
+        if not hasattr(analysis, 'branch_lines'):
+            # N.B. switching to the public method analysis.missing_branch_arcs
+            # would work for half of what we need, but there doesn't seem to be
+            # an equivalent analysis.executed_branch_arcs
+            branch_lines = analysis._branch_lines()  # pylint: disable=W0212
+        else:
+            branch_lines = analysis.branch_lines()
+
         branches = []
 
         for l1, l2 in analysis.arcs_executed():
