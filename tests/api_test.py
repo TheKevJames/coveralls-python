a2a71073d543268f5b6e4536f6ebf77cc1f9f88c
Jeshua Morrissey <jeshua@clipchamp.com>
2020-06-24 09:33:05 +1000

    .

--- tests/api_test.py
+++ tests/api_test.py
@@ -14,3 +14,17 @@ def test_output_to_file(tmpdir):
     report = test_log.read()
 
     assert json.loads(report)['repo_token'] == 'xxx'
+
+
+@mock.patch.dict(os.environ, {}, clear=True)
+def test_load_config_from_github():
+    """Check getting config from GH actions works when not in a PR."""
+    coveralls = Coveralls(repo_token='xxx')
+
+    os.environ['GITHUB_RUN_ID'] = 'run_id'
+    os.environ['GITHUB_REF'] = 'refs/push/somehash'
+    assert coveralls.load_config_from_github() == ('github-actions', 'run_id', None)
+
+    os.environ['GITHUB_RUN_ID'] = 'run_id'
+    os.environ['GITHUB_REF'] = 'refs/pull/123'
+    assert coveralls.load_config_from_github() == ('github-actions', 'run_id', '123')
