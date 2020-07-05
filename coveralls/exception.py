ba2a222e3fc6176674aa12109821498e2627c2e6
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2020-07-05 17:38:50 +0200

    Make CoverallsException comparable

--- coveralls/exception.py
+++ coveralls/exception.py
@@ -1,2 +1,8 @@
 class CoverallsException(Exception):
-    pass
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return str(self) == str(other)
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
