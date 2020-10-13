403870abfcb6f407c74a776ead642c90cd2dd738
Bruno Alla <browniebroke@users.noreply.github.com>
2020-10-13 17:54:59 +0100

    fix(semaphore): environment detection
    Improve earlier fix work with both Semaphore 2.0 and classic.

--- coveralls/api.py
+++ coveralls/api.py
@@ -118,9 +118,18 @@ class Coveralls:
 
     @staticmethod
     def load_config_from_semaphore():
-        job = os.environ.get('SEMAPHORE_CURRENT_JOB')
-        number = os.environ.get('SEMAPHORE_BUILD_NUMBER')
-        pr = os.environ.get('SEMAPHORE_BRANCH_ID')
+        job = (
+            os.environ.get('SEMAPHORE_JOB_UUID')  # Classic
+            or os.environ.get('SEMAPHORE_JOB_ID')  # 2.0
+        )
+        number = (
+            os.environ.get('SEMAPHORE_EXECUTABLE_UUID')  # Classic
+            or os.environ.get('SEMAPHORE_WORKFLOW_ID')  # 2.0
+        )
+        pr = (
+            os.environ.get('SEMAPHORE_BRANCH_ID')  # Classic
+            or os.environ.get('SEMAPHORE_GIT_PR_NUMBER')  # 2.0
+        )
         return 'semaphore-ci', job, number, pr
 
     @staticmethod
