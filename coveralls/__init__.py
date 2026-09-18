236699689ab1415c2b144bb5c725b5410ef86691
Kevin James <KevinJames@thekev.in>
2026-09-18 10:40:10 +0100

    chore: cleanup noqa format

--- coveralls/__init__.py
+++ coveralls/__init__.py
@@ -1,6 +1,7 @@
+# flake8: noqa: IMR241
 import importlib.metadata
 
-from .api import Coveralls  # noqa: IMR241
+from .api import Coveralls
 
 __version__ = importlib.metadata.version('coveralls')
 __all__ = ['Coveralls']
